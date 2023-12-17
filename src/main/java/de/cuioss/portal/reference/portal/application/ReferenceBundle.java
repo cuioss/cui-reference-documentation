a237a23c7420650ffc2670654f6a503ee6336b43
Oliver Wolff <23139298+cuioss@users.noreply.github.com>
2023-12-17 17:21:17 +0100

    More updates

--- src/main/java/de/cuioss/portal/reference/portal/application/ReferenceBundle.java
+++ src/main/java/de/cuioss/portal/reference/portal/application/ReferenceBundle.java
@@ -1,6 +1,9 @@
 package de.cuioss.portal.reference.portal.application;
 
+import java.util.Locale;
+import java.util.MissingResourceException;
 import java.util.Optional;
+import java.util.ResourceBundle;
 
 import javax.annotation.Priority;
 import javax.enterprise.context.ApplicationScoped;
@@ -19,7 +22,7 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode
 public class ReferenceBundle implements ResourceBundleLocator {
     private static final long serialVersionUID = -570736967563631611L;
-    private static final String PATH = "de.cuioss.portal.reference.i18n.reference-messages";
+    private static final String PATH = "de.cuioss.portal.reference.i18n.reference";
 
     @Override
     public Optional<String> getBundlePath() {
@@ -29,6 +32,25 @@ public class ReferenceBundle implements ResourceBundleLocator {
     @Override
     public String toString() {
         return "%s: Path='%s'".formatted(getClass().getName(), PATH);
+    }
+
+    /**
+     * This is needed in the context of a quarkus module: Otherwise the
+     * {@link ResourceBundle} can not be found. The difference to the default
+     * implementation is passing
+     * {@code Thread.currentThread().getContextClassLoader()}
+     */
+    @Override
+    public Optional<ResourceBundle> getBundle(Locale locale) {
+        try {
+            Optional<ResourceBundle> loadedBundle = Optional
+                    .of(ResourceBundle.getBundle(PATH, locale, Thread.currentThread().getContextClassLoader()));
+            LOGGER.debug("Successfully loaded %s '%s' for '%s'", getClass().getName(), PATH, locale);
+            return loadedBundle;
+        } catch (MissingResourceException e) {
+            LOGGER.warn("Unable to load %s '%s' for '%s'".formatted(getClass().getName(), PATH, locale), e);
+            return Optional.empty();
+        }
     }
 
 }
