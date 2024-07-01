f2d648664537d71d072f1b7da799e580194cae8e
Oliver Wolff <23139298+cuioss@users.noreply.github.com>
2024-07-01 17:18:32 +0200

    Enhance Documentation

--- src/main/java/de/cuioss/portal/reference/pages/components/demo/EncapsulateLazyLoadingPageBean.java
+++ src/main/java/de/cuioss/portal/reference/pages/components/demo/EncapsulateLazyLoadingPageBean.java
@@ -1,5 +1,6 @@
 package de.cuioss.portal.reference.pages.components.demo;
 
+import de.cuioss.tools.base.Preconditions;
 import de.cuioss.tools.logging.CuiLogger;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
@@ -9,6 +10,7 @@ import lombok.ToString;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -23,7 +25,7 @@ public class EncapsulateLazyLoadingPageBean implements Serializable {
     @Serial
     private static final long serialVersionUID = -1281930098669716629L;
 
-    private static final CuiLogger log = new CuiLogger(EncapsulateLazyLoadingPageBean.class);
+    private static final CuiLogger LOGGER = new CuiLogger(EncapsulateLazyLoadingPageBean.class);
 
     @Getter
     private boolean parentContainerRendered;
@@ -37,28 +39,25 @@ public class EncapsulateLazyLoadingPageBean implements Serializable {
     }
 
     public boolean isInitialized() {
-        if (!parentContainerRendered) {
-            throw new IllegalStateException(
-                "In case parent CC isn't rendered, this code shouldn't be invoked, " + "otherwise wrong behaviour");
-        }
+        Preconditions.checkState(parentContainerRendered, "In case parent CC isn't rendered, this code shouldn't be invoked, otherwise wrong behaviour");
         return false;
     }
 
     public void initialize() {
+        LOGGER.info("Entering initialize at %s", LocalDateTime.now());
         try {
             TimeUnit.SECONDS.sleep(3);
-            if (!parentContainerRendered) {
-                throw new IllegalStateException("In case parent CC isn't rendered, this code shouldn't be invoked, "
-                    + "otherwise wrong behaviour");
-            }
+            Preconditions.checkState(parentContainerRendered, "In case parent CC isn't rendered, this code shouldn't be invoked, otherwise wrong behaviour");
         } catch (InterruptedException e) {
-            log.error("interrupted: ", e);
+            LOGGER.error("interrupted: ", e);
             Thread.currentThread().interrupt();
         }
+        LOGGER.info("Leaving initialize at %s", LocalDateTime.now());
         this.content = mutableList("A", "B", "C");
     }
 
     public void switchOverParentContainerRendered() {
+        LOGGER.info("Entering switchOverParentContainerRendered at %s", LocalDateTime.now());
         this.parentContainerRendered = !parentContainerRendered;
     }
 }
